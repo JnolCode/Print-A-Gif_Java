34b635a986b875059ba57cbc720ada5dd660982f
JnolCode <Jnolcox.0429+Git@gmail.com>
2017-01-24 13:56:16 -0500

    Add files via upload

---
+++ PrintGif.java
@@ -0,0 +1,27 @@
+/**
+ * Print-A-Gif in Java
+ *
+ * Copyright 2017 by John Nolcox <jnolcox0429 at gmail.com>
+ *
+ * This is a Java replica of Stuart McConnels Print-A-Gif (Stupotmcdoodlepip) <stumcconnel at gmail.com>
+ *     Copyright 2017 by Stuart McConnell
+ *
+ * This file is part of Print-A-Gif
+ *
+ * Print-A-Gif is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Print-A-Gif is distributed in hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without the even implied warranty of
+ * MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have recieved a copy of the GNU General Public License
+ * along with Print-A-Gif. If not, see <http://www.gnu.org/lincenses/>
+ */
+
+public class PrintGif {
+    
+}
